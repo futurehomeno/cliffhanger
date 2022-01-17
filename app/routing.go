03d88e1a96b8f2ddb50feff289f9ef9fa842d600
Bogusław Wójcik <bogus.wojcik@gmail.com>
2022-01-17 16:11:57 +0100

    Todo

--- app/routing.go
+++ app/routing.go
@@ -40,6 +40,7 @@ func RouteApp(
 		RouteCmdAppGetManifest(serviceName, appLifecycle, configStorage, app),
 		RouteCmdConfigExtendedSet(serviceName, appLifecycle, configFactory, app, locker),
 		RouteCmdAppUninstall(serviceName, appLifecycle, app, locker),
+		// TODO: Implement reset and factory reset commands support.
 	}
 }
 
