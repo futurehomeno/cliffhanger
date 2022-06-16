2c6fb7b80e58aa548f5ae94245d3532bef6eb6ee
markushaldorsen <markushaldorsen@gmail.com>
2022-06-16 11:35:05 +0200

    Increase test coverage of routing.go

--- adapter/thing/light_test.go
+++ adapter/thing/light_test.go
@@ -112,6 +112,39 @@ func TestRouteLight(t *testing.T) { //nolint:paralleltest
 							suite.ExpectError("pt:j1/mt:evt/rt:dev/rn:test_adapter/ad:1/sv:out_lvl_switch/ad:2", "out_lvl_switch"),
 						},
 					},
+					{
+						Name:    "wrong value type",
+						Command: suite.StringMessage("pt:j1/mt:cmd/rt:dev/rn:test_adapter/ad:1/sv:out_lvl_switch/ad:2", "cmd.lvl.set", "out_lvl_switch", "99"),
+						Expectations: []*suite.Expectation{
+							suite.ExpectError("pt:j1/mt:evt/rt:dev/rn:test_adapter/ad:1/sv:out_lvl_switch/ad:2", "out_lvl_switch"),
+						},
+					},
+					{
+						Name:    "wrong address",
+						Command: suite.IntMessage("pt:j1/mt:cmd/rt:dev/rn:test_adapter/ad:1/sv:out_lvl_switch/ad:3", "cmd.lvl.set", "out_lvl_switch", 99),
+						Expectations: []*suite.Expectation{
+							suite.ExpectError("pt:j1/mt:evt/rt:dev/rn:test_adapter/ad:1/sv:out_lvl_switch/ad:3", "out_lvl_switch"),
+						},
+					},
+				},
+			},
+			{
+				Name: "failed set level with duration - setting error",
+				Setup: routeLight(
+					mockedoutlvlswitch.NewController(t).
+						MockSetLevelSwitchLevelWithDuration(99, 1, errors.New("setting error"), false),
+				),
+				Nodes: []*suite.Node{
+					{
+						Name: "set level with duration",
+						Command: suite.NewMessageBuilder().
+							IntMessage("pt:j1/mt:cmd/rt:dev/rn:test_adapter/ad:1/sv:out_lvl_switch/ad:2", "cmd.lvl.set", "out_lvl_switch", 99).
+							AddProperty(outlvlswitch.Duration, "1").
+							Build(),
+						Expectations: []*suite.Expectation{
+							suite.ExpectError("pt:j1/mt:evt/rt:dev/rn:test_adapter/ad:1/sv:out_lvl_switch/ad:2", "out_lvl_switch"),
+						},
+					},
 				},
 			},
 			{
@@ -163,6 +196,20 @@ func TestRouteLight(t *testing.T) { //nolint:paralleltest
 							suite.ExpectError("pt:j1/mt:evt/rt:dev/rn:test_adapter/ad:1/sv:out_lvl_switch/ad:2", "out_lvl_switch"),
 						},
 					},
+					{
+						Name:    "wrong value type",
+						Command: suite.StringMessage("pt:j1/mt:cmd/rt:dev/rn:test_adapter/ad:1/sv:out_lvl_switch/ad:2", "cmd.binary.set", "out_lvl_switch", "true"),
+						Expectations: []*suite.Expectation{
+							suite.ExpectError("pt:j1/mt:evt/rt:dev/rn:test_adapter/ad:1/sv:out_lvl_switch/ad:2", "out_lvl_switch"),
+						},
+					},
+					{
+						Name:    "wrong address",
+						Command: suite.BoolMessage("pt:j1/mt:cmd/rt:dev/rn:test_adapter/ad:1/sv:out_lvl_switch/ad:3", "cmd.binary.set", "out_lvl_switch", true),
+						Expectations: []*suite.Expectation{
+							suite.ExpectError("pt:j1/mt:evt/rt:dev/rn:test_adapter/ad:1/sv:out_lvl_switch/ad:3", "out_lvl_switch"),
+						},
+					},
 				},
 			},
 			{
@@ -176,6 +223,39 @@ func TestRouteLight(t *testing.T) { //nolint:paralleltest
 					{
 						Name:    "set binary",
 						Command: suite.BoolMessage("pt:j1/mt:cmd/rt:dev/rn:test_adapter/ad:1/sv:out_lvl_switch/ad:2", "cmd.binary.set", "out_lvl_switch", true),
+						Expectations: []*suite.Expectation{
+							suite.ExpectError("pt:j1/mt:evt/rt:dev/rn:test_adapter/ad:1/sv:out_lvl_switch/ad:2", "out_lvl_switch"),
+						},
+					},
+				},
+			},
+			{
+				Name: "failed get level - send level error",
+				Setup: routeLight(
+					mockedoutlvlswitch.NewController(t).
+						MockLevelSwitchLevelReport(99, errors.New("sending error"), false),
+				),
+				Nodes: []*suite.Node{
+					{
+						Name:    "get level",
+						Command: suite.NullMessage("pt:j1/mt:cmd/rt:dev/rn:test_adapter/ad:1/sv:out_lvl_switch/ad:2", "cmd.lvl.get_report", "out_lvl_switch"),
+						Expectations: []*suite.Expectation{
+							suite.ExpectError("pt:j1/mt:evt/rt:dev/rn:test_adapter/ad:1/sv:out_lvl_switch/ad:2", "out_lvl_switch"),
+						},
+					},
+				},
+			},
+			{
+				Name: "failed get level - send binary error",
+				Setup: routeLight(
+					mockedoutlvlswitch.NewController(t).
+						MockLevelSwitchLevelReport(99, nil, false).
+						MockLevelSwitchBinaryReport(true, errors.New("sending error"), false),
+				),
+				Nodes: []*suite.Node{
+					{
+						Name:    "get level",
+						Command: suite.NullMessage("pt:j1/mt:cmd/rt:dev/rn:test_adapter/ad:1/sv:out_lvl_switch/ad:2", "cmd.lvl.get_report", "out_lvl_switch"),
 						Expectations: []*suite.Expectation{
 							suite.ExpectError("pt:j1/mt:evt/rt:dev/rn:test_adapter/ad:1/sv:out_lvl_switch/ad:2", "out_lvl_switch"),
 						},
