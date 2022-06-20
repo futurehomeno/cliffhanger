0c5cee57af6f9cca4dbd6e5069ccbcdf3d8ed132
markushaldorsen <markushaldorsen@gmail.com>
2022-06-20 13:19:24 +0200

    Init presence implementation

---
+++ adapter/service/presence/routing.go
@@ -0,0 +1,57 @@
+package presence
+
+import (
+	"fmt"
+
+	"github.com/futurehomeno/fimpgo"
+
+	"github.com/futurehomeno/cliffhanger/adapter"
+	"github.com/futurehomeno/cliffhanger/router"
+)
+
+const (
+	CmdPresenceGetReport = "cmd.presence.get_report"
+	EvtPresenceReport    = "evt.presence.report"
+
+	Presence = "presence"
+)
+
+// RouteService returns routing for service specific commands.
+func RouteService(adapter adapter.Adapter) []*router.Routing {
+	return []*router.Routing{
+		RouteCmdPresenceGetReport(adapter),
+	}
+}
+
+// RouteCmdPresenceGetReport returns a routing responsible for handling the command.
+func RouteCmdPresenceGetReport(adapter adapter.Adapter) *router.Routing {
+	return router.NewRouting(
+		HandleCmdPresenceGetReport(adapter),
+		router.ForService(Presence),
+		router.ForType(CmdPresenceGetReport),
+	)
+}
+
+// HandleCmdPresenceGetReport returns a handler responsible for handling the command.
+func HandleCmdPresenceGetReport(adapter adapter.Adapter) router.MessageHandler {
+	return router.NewMessageHandler(
+		router.MessageProcessorFn(func(message *fimpgo.Message) (*fimpgo.FimpMessage, error) {
+			s := adapter.ServiceByTopic(message.Topic)
+			if s == nil {
+				return nil, fmt.Errorf("adapter: service not found under the provided address: %s", message.Addr.ServiceAddress)
+			}
+
+			presence, ok := s.(Service)
+			if !ok {
+				return nil, fmt.Errorf("adapter: incorrect service found under the provided address: %s", message.Addr.ServiceAddress)
+			}
+
+			_, err := presence.SendPresenceReport(true)
+			if err != nil {
+				return nil, fmt.Errorf("adapter: failed to send presence report: %w", err)
+			}
+
+			return nil, nil
+		}),
+	)
+}
---
+++ adapter/service/presence/specification.go
@@ -0,0 +1,44 @@
+package presence
+
+import (
+	"fmt"
+
+	"github.com/futurehomeno/fimpgo"
+	"github.com/futurehomeno/fimpgo/fimptype"
+)
+
+// Specification creates a service specification.
+func Specification(
+	resourceName,
+	resourceAddress,
+	address,
+	groups []string,
+) *fimptype.Service {
+	return &fimptype.Service{
+		Address: fmt.Sprintf("/rt:dev/rn:%s/ad:%s/sv:%s/ad:%s", resourceName, resourceAddress, Presence, address),
+		Name:    Presence,
+
+		Groups:     groups,
+		Enabled:    true,
+		Props:      nil,
+		Interfaces: requiredInterfaces(),
+	}
+}
+
+// requiredInterfaces returns required interfaces by the service.
+func requiredInterfaces() []fimptype.Interface {
+	return []fimptype.Interface{
+		{
+			Type:      fimptype.TypeIn,
+			MsgType:   CmdPresenceGetReport,
+			ValueType: fimpgo.VTypeNull,
+			Version:   "1",
+		},
+		{
+			Type:      fimptype.TypeOut,
+			MsgType:   EvtPresenceReport,
+			ValueType: fimpgo.VTypeBool,
+			Version:   "1",
+		},
+	}
+}
