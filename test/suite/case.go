c8c9749685251f309db52adc4b7f60c7376a035f
Radosław Strączyński <radek.straczynski@gmail.com>
2022-10-20 10:25:57 +0200

    Reversed order of test suite's case callback injection (#56)

--- test/suite/case.go
+++ test/suite/case.go
@@ -118,9 +118,9 @@ func (c *Case) init(t *testing.T, mqtt *fimpgo.MqttTransport) {
 		return
 	}
 
-	c.initRouting(mqtt)
-	c.initTasks()
 	c.initService()
+	c.initTasks()
+	c.initRouting(mqtt)
 }
 
 func (c *Case) initRouting(mqtt *fimpgo.MqttTransport) {
@@ -196,13 +196,13 @@ func (c *Case) initService() {
 // injectCallbacks is responsible for queuing callbacks for execution at the correct stage of the test case.
 // All initialization callbacks must execute only after a first node has already set up its expectations to avoid a race condition.
 // For this reason all initialization callbacks are injected into the first node which is responsible for running them at a correct moment.
+// The callbacks are prepended to allow the first node to make assumptions against the environment (e.g. ensuring if the Service is already running).
 func (c *Case) injectCallbacks(initCallback Callback, tearDownCallback Callback) {
-
 	firstNode := c.Nodes[0]
 
-	firstNode.InitCallbacks = append(c.Nodes[0].InitCallbacks, initCallback)
+	firstNode.InitCallbacks = append([]Callback{initCallback}, c.Nodes[0].InitCallbacks...)
 
-	c.TearDown = append(c.TearDown, tearDownCallback)
+	c.TearDown = append([]Callback{tearDownCallback}, c.TearDown...)
 }
 
 func (c *Case) tearDown(t *testing.T) {
