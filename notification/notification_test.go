6853abd70efb3c276d5f2b82a01323244330ff15
Bogusław Wójcik <31538120+boguslaw-wojcik@users.noreply.github.com>
2022-11-16 08:50:14 +0100

    Notification refactor.

---
+++ notification/notification_test.go
@@ -0,0 +1,88 @@
+package notification_test
+
+import (
+	"testing"
+
+	"github.com/futurehomeno/fimpgo"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/futurehomeno/cliffhanger/notification"
+	"github.com/futurehomeno/cliffhanger/router"
+	"github.com/futurehomeno/cliffhanger/task"
+	"github.com/futurehomeno/cliffhanger/test/suite"
+)
+
+func TestNotification(t *testing.T) { //nolint:paralleltest
+	var service notification.Notification
+
+	s := &suite.Suite{
+		Cases: []*suite.Case{
+			{
+				Name: "Notification",
+				Setup: suite.BaseSetup(func(t *testing.T, mqtt *fimpgo.MqttTransport) (routing []*router.Routing, tasks []*task.Task, mocks []suite.Mock) {
+					t.Helper()
+
+					service = notification.NewNotification(mqtt)
+
+					return nil, nil, nil
+				}),
+				Nodes: []*suite.Node{
+					{
+						Name: "Event",
+						Callbacks: []suite.Callback{
+							func(t *testing.T) {
+								t.Helper()
+
+								err := service.Event(&notification.Event{
+									EventName: "test_event_name",
+								})
+
+								assert.NoError(t, err)
+							},
+						},
+						Expectations: []*suite.Expectation{
+							suite.ExpectStringMap("pt:j1/mt:evt/rt:app/rn:kind_owl/ad:1", "evt.notification.report", "kind_owl", map[string]string{
+								"EventName":      "test_event_name",
+								"MessageContent": "",
+								"DeviceId":       "",
+								"DeviceName":     "",
+								"RoomId":         "",
+								"RoomName":       "",
+								"AreaId":         "",
+								"AreaName":       "",
+								"AreaType":       "",
+							}),
+						},
+					},
+					{
+						Name: "Message",
+						Callbacks: []suite.Callback{
+							func(t *testing.T) {
+								t.Helper()
+
+								err := service.Message("custom test message notification")
+
+								assert.NoError(t, err)
+							},
+						},
+						Expectations: []*suite.Expectation{
+							suite.ExpectStringMap("pt:j1/mt:evt/rt:app/rn:kind_owl/ad:1", "evt.notification.report", "kind_owl", map[string]string{
+								"EventName":      "custom",
+								"MessageContent": "custom test message notification",
+								"DeviceId":       "",
+								"DeviceName":     "",
+								"RoomId":         "",
+								"RoomName":       "",
+								"AreaId":         "",
+								"AreaName":       "",
+								"AreaType":       "",
+							}),
+						},
+					},
+				},
+			},
+		},
+	}
+
+	s.Run(t)
+}
---
+++ notification/timeline_test.go
@@ -0,0 +1,83 @@
+package notification_test
+
+import (
+	"testing"
+
+	"github.com/futurehomeno/fimpgo"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/futurehomeno/cliffhanger/notification"
+	"github.com/futurehomeno/cliffhanger/router"
+	"github.com/futurehomeno/cliffhanger/task"
+	"github.com/futurehomeno/cliffhanger/test/suite"
+)
+
+func TestTimeline(t *testing.T) { //nolint:paralleltest
+	var service notification.Timeline
+
+	s := &suite.Suite{
+		Cases: []*suite.Case{
+			{
+				Name: "Timeline",
+				Setup: suite.BaseSetup(func(t *testing.T, mqtt *fimpgo.MqttTransport) (routing []*router.Routing, tasks []*task.Task, mocks []suite.Mock) {
+					t.Helper()
+
+					service = notification.NewTimeline(mqtt)
+
+					return nil, nil, nil
+				}),
+				Nodes: []*suite.Node{
+					{
+						Name: "Event",
+						Callbacks: []suite.Callback{
+							func(t *testing.T) {
+								t.Helper()
+
+								err := service.Event(&notification.TimelineEvent{
+									EventName: "test_event_name",
+								})
+
+								assert.NoError(t, err)
+							},
+						},
+						Expectations: []*suite.Expectation{
+							suite.ExpectStringMap("pt:j1/mt:cmd/rt:app/rn:time_owl/ad:1", "cmd.timeline.set", "kind_owl", map[string]string{
+								"EventName":      "test_event_name",
+								"MessageContent": "",
+								"DeviceId":       "",
+								"DeviceName":     "",
+								"RoomName":       "",
+								"AreaName":       "",
+							}),
+						},
+					},
+					{
+						Name: "Message",
+						Callbacks: []suite.Callback{
+							func(t *testing.T) {
+								t.Helper()
+
+								err := service.Message(&notification.TimelineMessage{
+									Sender:    "test_sender",
+									MessageEN: "test_message_en",
+									MessageNO: "test_message_no",
+								})
+
+								assert.NoError(t, err)
+							},
+						},
+						Expectations: []*suite.Expectation{
+							suite.ExpectStringMap("pt:j1/mt:cmd/rt:app/rn:time_owl/ad:1", "cmd.timeline.set", "time_owl", map[string]string{
+								"sender":     "test_sender",
+								"message_en": "test_message_en",
+								"message_no": "test_message_no",
+							}),
+						},
+					},
+				},
+			},
+		},
+	}
+
+	s.Run(t)
+}
