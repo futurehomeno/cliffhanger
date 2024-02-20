c24ba9fd7de35bbcf94ac1aab6425d41bf725524
Rafał Mnich <rafal@futurehome.no>
2024-02-20 11:58:55 +0100

    DEV-2656 Added out_bin_switch service.

--- adapter/service/outbinswitch/routing.go
+++ adapter/service/outbinswitch/routing.go
@@ -1,5 +1,14 @@
 package outbinswitch
 
+import (
+	"fmt"
+
+	"github.com/futurehomeno/fimpgo"
+
+	"github.com/futurehomeno/cliffhanger/adapter"
+	"github.com/futurehomeno/cliffhanger/router"
+)
+
 // Constants defining routing service, commands and events.
 const (
 	CmdBinarySet       = "cmd.binary.set"
@@ -8,3 +17,87 @@ const (
 
 	OutBinSwitch = "out_bin_switch"
 )
+
+// RouteService returns routing for service specific commands.
+func RouteService(serviceRegistry adapter.ServiceRegistry) []*router.Routing {
+	return []*router.Routing{
+		RouteCmdBinarySet(serviceRegistry),
+		RouteCmdBinaryGetReport(serviceRegistry),
+	}
+}
+
+// RouteCmdBinarySet returns a routing responsible for handling the command.
+func RouteCmdBinarySet(serviceRegistry adapter.ServiceRegistry) *router.Routing {
+	return router.NewRouting(
+		HandleCmdBinarySet(serviceRegistry),
+		router.ForService(OutBinSwitch),
+		router.ForType(CmdBinarySet),
+	)
+}
+
+// RouteCmdBinaryGetReport returns a routing responsible for handling the command.
+func RouteCmdBinaryGetReport(serviceRegistry adapter.ServiceRegistry) *router.Routing {
+	return router.NewRouting(
+		HandleCmdBinaryGetReport(serviceRegistry),
+		router.ForService(OutBinSwitch),
+		router.ForType(CmdBinaryGetReport),
+	)
+}
+
+// HandleCmdBinarySet returns a handler responsible for handling the command.
+func HandleCmdBinarySet(serviceRegistry adapter.ServiceRegistry) router.MessageHandler {
+	return router.NewMessageHandler(
+		router.MessageProcessorFn(func(message *fimpgo.Message) (*fimpgo.FimpMessage, error) {
+			s := serviceRegistry.ServiceByTopic(message.Topic)
+			if s == nil {
+				return nil, fmt.Errorf("adapter: service not found under the provided address: %s", message.Addr.ServiceAddress)
+			}
+
+			outBinSwitch, ok := s.(Service)
+			if !ok {
+				return nil, fmt.Errorf("adapter: incorrect service found under the provided address: %s", message.Addr.ServiceAddress)
+			}
+
+			value, err := message.Payload.GetBoolValue()
+			if err != nil {
+				return nil, fmt.Errorf("adapter: failed to get value from the message: %w", err)
+			}
+
+			err = outBinSwitch.SetBinaryState(value)
+			if err != nil {
+				return nil, fmt.Errorf("adapter: failed to set state: %w", err)
+			}
+
+			_, err = outBinSwitch.SendBinaryReport(true)
+			if err != nil {
+				return nil, fmt.Errorf("adapter: failed to send binary report: %w", err)
+			}
+
+			return nil, nil
+		}),
+	)
+}
+
+// HandleCmdBinaryGetReport returns a handler responsible for handling the command.
+func HandleCmdBinaryGetReport(serviceRegistry adapter.ServiceRegistry) router.MessageHandler {
+	return router.NewMessageHandler(
+		router.MessageProcessorFn(func(message *fimpgo.Message) (*fimpgo.FimpMessage, error) {
+			s := serviceRegistry.ServiceByTopic(message.Topic)
+			if s == nil {
+				return nil, fmt.Errorf("adapter: service not found under the provided address: %s", message.Addr.ServiceAddress)
+			}
+
+			outBinSwitch, ok := s.(Service)
+			if !ok {
+				return nil, fmt.Errorf("adapter: incorrect service found under the provided address: %s", message.Addr.ServiceAddress)
+			}
+
+			_, err := outBinSwitch.SendBinaryReport(true)
+			if err != nil {
+				return nil, fmt.Errorf("adapter: failed to send binary report: %w", err)
+			}
+
+			return nil, nil
+		}),
+	)
+}
