8863bde4a4bf04713c0a8ea47c556904324be1df
budzik <michal@futurehome.no>
2026-04-22 11:07:24 +0200

    WIP

--- discovery/model.go
+++ discovery/model.go
@@ -1,14 +1,18 @@
 package discovery
 
+import "github.com/futurehomeno/cliffhanger/lifecycle"
+
 // Constants defining resource types.
 const (
 	ResourceTypeApp = "app"
 	ResourceTypeAd  = "ad"
 )
 
-type Resource struct {
-	ResourceName string `json:"resource_name"` // Name of the application or adapter.
-	ResourceType string `json:"resource_type"` // Type of the service: "app", "ad"
-	InstanceID   string `json:"instance_id"`   // An instance ID of the service, usually 1.
-	Version      string `json:"version"`       // Version of the application.
+type ResourceT struct {
+	ResourceName string              `json:"resource_name"`        // Name of the application or adapter.
+	ResourceType string              `json:"resource_type"`        // Type of the service: "app", "ad"
+	PackageName  string              `json:"package_name"`         // Package name may be different from the resource name.
+	InstanceID   string              `json:"instance_id"`          // An instance ID of the service, usually 1.
+	Version      string              `json:"version"`              // Version of the application.
+	States       lifecycle.AppStates `json:"app_states,omitempty"` // Current states of the application.
 }
