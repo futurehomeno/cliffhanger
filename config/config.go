4362526787dc0ceff91393bfe1d421e77fa142bc
budzik <michal@futurehome.no>
2026-04-23 11:12:56 +0200

    ifix

--- config/config.go
+++ config/config.go
@@ -5,25 +5,25 @@ import "time"
 const configFileName = "config.json"
 
 type Default struct {
-	WorkDir             string    `json:"-"`
-	ConfigDir           string    `json:"-"`
-	ConfigVersion       string    `json:"config_version,omitempty"`
-	MQTTServerURI       string    `json:"mqtt_server_uri"`
-	MQTTUsername        string    `json:"mqtt_server_username"`
-	MQTTPassword        string    `json:"mqtt_server_password"`
-	MQTTClientIDPrefix  string    `json:"mqtt_client_id_prefix"`
-	InfoFile            string    `json:"info_file"`
-	LogFile             string    `json:"log_file"`
-	LogLevel            string    `json:"log_level"`
-	LogFormat           string    `json:"log_format"`
-	LogRevertTimeout    string    `json:"log_revert_timeout,omitempty"`
-	LogRevertAt         time.Time `json:"log_revert_at"`
-	RestartsCount       int       `json:"restarts_count,omitempty"`
-	TelemetryEnabled    *bool     `json:"telemetry_enabled,omitempty"`
-	TelemetryEnabledAt  string    `json:"telemetry_enabled_at,omitempty"`
-	TelemetryValidity   string    `json:"telemetry_validity,omitempty"`
-	TelemetrySuppressed *bool     `json:"telemetry_suppressed,omitempty"`
-	ConfiguredAt        string    `json:"configured_at"`
+	WorkDir             string        `json:"-"`
+	ConfigDir           string        `json:"-"`
+	ConfigVersion       string        `json:"config_version,omitempty"`
+	MQTTServerURI       string        `json:"mqtt_server_uri"`
+	MQTTUsername        string        `json:"mqtt_server_username"`
+	MQTTPassword        string        `json:"mqtt_server_password"`
+	MQTTClientIDPrefix  string        `json:"mqtt_client_id_prefix"`
+	InfoFile            string        `json:"info_file"`
+	LogFile             string        `json:"log_file"`
+	LogLevel            string        `json:"log_level"`
+	LogFormat           string        `json:"log_format"`
+	LogRevertTimeout    time.Duration `json:"log_revert_timeout,omitempty"`
+	LogRevertAt         time.Time     `json:"log_revert_at"`
+	RestartsCount       int           `json:"restarts_count,omitempty"`
+	TelemetryEnabled    *bool         `json:"telemetry_enabled,omitempty"`
+	TelemetryEnabledAt  string        `json:"telemetry_enabled_at,omitempty"`
+	TelemetryValidity   string        `json:"telemetry_validity,omitempty"`
+	TelemetrySuppressed *bool         `json:"telemetry_suppressed,omitempty"`
+	ConfiguredAt        string        `json:"configured_at"`
 }
 
 func NewDefault(workDir string) Default {
